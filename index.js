a7cf248d2c265431f4f75d51ee8f54ac3c8e6482
TZ <atian25@qq.com>
2014-11-07 13:12:41 +0800

    修改github地址, 并注册到npm
    Signed-off-by: TZ <atian25@qq.com>

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var fis = module.exports = require('fis');
 var pkg = fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
-fis.require.prefixes = [ pkg.name, 'scrat', 'fis' ];
-fis.cli.name = pkg.name;
+fis.cli.name = 'larva';
+
+fis.require.prefixes = [ fis.cli.name, 'scrat', 'fis' ];
 fis.cli.version = require('./version.js');
 fis.cli.help.commands = [ 'release', 'install', 'server', 'init' ];
 
@@ -12,7 +13,7 @@ var larvaConfig = require('./configs/larva.js');
 fis.config.merge(larvaConfig);
 
 //alias
-Object.defineProperty(global, pkg.name, {
+Object.defineProperty(global, fis.cli.name, {
   enumerable : true,
   writable : false,
   value : fis
