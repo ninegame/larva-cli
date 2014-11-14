2af7fda093c6e76b2598fb27e1707e9f2602b482
TZ <atian25@qq.com>
2014-11-14 18:21:15 +0800

    试着干掉路径
    Signed-off-by: TZ <atian25@qq.com>

--- configs/larva.js
+++ configs/larva.js
@@ -1,7 +1,7 @@
 module.exports = {
   framework: {
     //cache: true,
-    urlPattern: '/c/%s',
+    urlPattern: '/%s',
     comboPattern: '/co??%s'
   },
   project: {
@@ -11,24 +11,134 @@ module.exports = {
     deploy: ['default', 'pack']
   },
   roadmap: {
-    path: [{
-      reg: /\/__[^\/]+$/,
-      release: false
-    },{
-      reg: /.*\.tpl\.html$/,
-      release: false
-    },{
-      reg: /.*\.spec\.js$/,
-      release: false
-    },{
-      reg: /([^\/]+\.inject\.js)$/i,
-      release: '/server/inject/$1',
-      useHash: false
-    },{
-      reg: "package.json",
-      release: '/public/package.json',
-      useHash: false
-    }]
+    path: [
+      {
+        reg: /.*\.tpl\.html$/,
+        release: false
+      },
+      {
+        reg: /.*\.spec\.js$/,
+        release: false
+      },
+      {
+        reg: /([^\/]+\.inject\.js)$/i,
+        release: '/server/inject/$1',
+        useHash: false
+      },
+      {
+        reg : '**.handlebars',
+        release : false,
+        isJsLike : true
+      },
+      {
+        reg : '**.md',
+        release : false,
+        isHtmlLike : true
+      },
+      {
+        reg : /\.inline\.\w+$/i,
+        release : false
+      },
+      {
+        reg : '**.jade'
+      },
+      {
+        reg : /^\/component_modules\/(.*\.tpl)$/i,
+        isHtmlLike : true,
+        release : '/views/c/$1'
+      },
+      {
+        reg : /^\/components\/(.*\.tpl)$/i,
+        isHtmlLike : true,
+        release : '/views/c/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/views\/(.*\.tpl)$/,
+        useCache : false,
+        isViews : true,
+        isHtmlLike : true,
+        release : '/views/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/component_modules\/(.*)\.(styl|css)$/i,
+        id : '$1.css',
+        isMod : true,
+        useSprite : true,
+        useHash : false,
+        url : '${urlPrefix}/c/$1.$2',
+        release : '/public/c/$1.$2'
+      },
+      {
+        reg : /^\/component_modules\/(.*\.js)$/i,
+        id : '$1',
+        isMod : true,
+        useHash : false,
+        url : '${urlPrefix}/c/$1',
+        release : '/public/c/$1'
+      },
+      {
+        reg : /^\/component_modules\/(.*)$/i,
+        url : '${urlPrefix}/c/$1',
+        release : '/public/c/$1'
+      },
+      {
+        reg : /^\/components\/(.*)\.(styl|css)$/i,
+        id : '${name}/${version}/$1.css',
+        isMod : true,
+        useSprite : true,
+        useHash : false,
+        url : '${urlPrefix}/c/${name}/${version}/$1.$2',
+        release : '/public/c/${name}/${version}/$1.$2'
+      },
+      {
+        reg : /^\/components\/(.*\.js)$/i,
+        id : '${name}/${version}/$1',
+        isMod : true,
+        isComponent : true,
+        useHash : false,
+        url : '${urlPrefix}/c/${name}/${version}/$1',
+        release : '/public/c/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/components\/(.*)$/i,
+        url : '${urlPrefix}/c/${name}/${version}/$1',
+        release : '/public/c/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/views\/(.*\.(?:html?|js))$/,
+        useCache : false,
+        isViews : true,
+        url : '${urlPrefix}/${name}/${version}/$1',
+        release : '/public/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/views\/(.*)$/,
+        useSprite : true,
+        isViews : true,
+        url : '${urlPrefix}/${name}/${version}/$1',
+        release : '/public/${name}/${version}/$1'
+      },
+      {
+        reg : /^\/public\/(.*)$/,
+        useSprite : true,
+        url : '${urlPrefix}/${name}/${version}/$1',
+        release : '/public/${name}/${version}/$1'
+      },
+      {
+        reg : 'map.json',
+        release : false
+      },
+      {
+        reg : '**',
+        useHash : false,
+        useCompile : false
+      }
+    //  {
+    //  reg: "package.json",
+    //  release: '/public/package.json',
+    //  useHash: false
+    //}
+    ]
   },
   settings: {
     optimizer : {
--- index.js
+++ index.js
@@ -12,6 +12,132 @@ fis.config.merge(defaultConfig);
 var larvaConfig = require('./configs/larva.js');
 fis.config.merge(larvaConfig);
 
+console.log('xxxx' + fis.config.get('name'))
+
+fis.config.set('roadmap.path', [
+  {
+    reg: /.*\.tpl\.html$/,
+    release: false
+  },
+  {
+    reg: /.*\.spec\.js$/,
+    release: false
+  },
+  {
+    reg: /([^\/]+\.inject\.js)$/i,
+    release: '/server/inject/$1',
+    useHash: false
+  },
+  {
+    reg : '**.handlebars',
+    release : false,
+    isJsLike : true
+  },
+  {
+    reg : '**.md',
+    release : false,
+    isHtmlLike : true
+  },
+  {
+    reg : /\.inline\.\w+$/i,
+    release : false
+  },
+  {
+    reg : '**.jade'
+  },
+  //{
+  //  reg : /^\/component_modules\/(.*\.tpl)$/i,
+  //  isHtmlLike : true,
+  //  release : '/views/c/$1'
+  //},
+  //{
+  //  reg : /^\/components\/(.*\.tpl)$/i,
+  //  isHtmlLike : true,
+  //  release : '/views/c/${name}/${version}/$1'
+  //},
+  //{
+  //  reg : /^\/views\/(.*\.tpl)$/,
+  //  useCache : false,
+  //  isViews : true,
+  //  isHtmlLike : true,
+  //  release : '/views/${name}/${version}/$1'
+  //},
+  //{
+  //  reg : /^\/component_modules\/(.*)\.(styl|css)$/i,
+  //  id : '$1.css',
+  //  isMod : true,
+  //  useSprite : true,
+  //  useHash : false,
+  //  url : '${urlPrefix}/c/$1.$2',
+  //  release : '/public/c/$1.$2'
+  //},
+  //{
+  //  reg : /^\/component_modules\/(.*\.js)$/i,
+  //  id : '$1',
+  //  isMod : true,
+  //  useHash : false,
+  //  url : '${urlPrefix}/c/$1',
+  //  release : '/public/c/$1'
+  //},
+  //{
+  //  reg : /^\/component_modules\/(.*)$/i,
+  //  url : '${urlPrefix}/c/$1',
+  //  release : '/public/c/$1'
+  //},
+  {
+    reg : /^\/components\/(.*)\.(styl|css)$/i,
+    id : '${name}/${version}/c/$1.css',
+    isMod : true,
+    useSprite : true,
+    useHash : false,
+    url : '${urlPrefix}/${name}/${version}/c/$1.$2',
+    release : '/public/${name}/${version}/c/$1.$2'
+  },
+  {
+    reg : /^\/components\/(.*\.js)$/i,
+    id : '${name}/${version}/c/$1',
+    isMod : true,
+    isComponent : true,
+    useHash : false,
+    url : '${urlPrefix}/${name}/${version}/c/$1',
+    release : '/public/${name}/${version}/c/$1'
+  },
+  {
+    reg : /^\/components\/(.*)$/i,
+    url : '${urlPrefix}/${name}/${version}/c/$1',
+    release : '/public/${name}/${version}/c/$1'
+  },
+  {
+    reg : /^\/views\/(.*\.(?:html?|js))$/,
+    useCache : false,
+    isViews : true,
+    url : '${urlPrefix}/${name}/${version}/$1',
+    release : '/public/${name}/${version}/$1'
+  },
+  {
+    reg : /^\/views\/(.*)$/,
+    useSprite : true,
+    isViews : true,
+    url : '${urlPrefix}/${name}/${version}/$1',
+    release : '/public/${name}/${version}/$1'
+  },
+  //{
+  //  reg : /^\/public\/(.*)$/,
+  //  useSprite : true,
+  //  url : '${urlPrefix}/${name}/${version}/$1',
+  //  release : '/public/${name}/${version}/$1'
+  //},
+  {
+    reg : 'map.json',
+    release : false
+  },
+  {
+    reg : '**',
+    useHash : false,
+    useCompile : false
+  }
+]);
+
 fis.require._cache['command-publish'] = {
   name: 'publish',
   desc: 'short cmd for release',
@@ -22,6 +148,19 @@ fis.require._cache['command-publish'] = {
         var argv = process.argv;
         argv.splice(2, 1, 'release', '-opm');
         fis.cli.run(argv);
+      });
+  }
+};
+
+fis.require._cache['command-test'] = {
+  name: 'test',
+  desc: 'short cmd for release',
+  register: function(commander){
+    commander
+      .action(function() {
+        var mainBowerFiles = require('main-bower-files');
+        var files = mainBowerFiles(/* options */);
+        console.log(files)
       });
   }
 };
